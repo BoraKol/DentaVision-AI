a66de50cc688e94a332b127c72fc47df78a142b6
Bora Kol <borakol07@gmail.com>
2026-03-26 23:38:22 +0300

    cleanup complete android for now

--- android/app/src/main/java/com/dentavision/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.dentavision.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dentavision.app',
-  appName: 'DentaVisionAI',
-  webDir: 'dist'
-};
-
-export default config;
